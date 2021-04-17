1fb48bd116d2be3b6290b5c3a92eb02de8a12cf1
Pradheepa <Pradheepa_P@discovery.com>
2021-04-17 19:47:28 +0800

    :clock: Scheduler Event Added for every 2 minutes

--- handler.js
+++ handler.js
@@ -1,18 +1,9 @@
 'use strict';
 
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log('EVENT: Lambda scheduler with CloudWatch Events');
+  console.log('EVENT DETAILS:', JSON.stringify(event));
+
+  return { message: 'CloudWatch Scheduler Event Triggered', event };
 };
